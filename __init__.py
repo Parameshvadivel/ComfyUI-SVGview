f9b8c14bd1805518ef4efcc1fddaae9cf3dce21f
Parameswaran <parameswaranv@outlook.com>
2024-07-28 18:10:00 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .svgview import *
+
+NODE_CLASS_MAPPINGS = {
+    "SVGPreview": SVGPreview
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SVGPreview": "Preview SVG"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
